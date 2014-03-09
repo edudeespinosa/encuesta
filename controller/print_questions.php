8fd9703525b3de1413579e1dbe1ecb7775ff4ca6
edudeespinosa <edude.espinosa@gmail.com>
2014-03-09 00:06:28 -0600

    ijndfnad

--- controller/print_questions.php
+++ controller/print_questions.php
@@ -2,8 +2,13 @@
 
 	$pregunta = $_POST["numPregunta"];
 	include_once("../models/modelo.php");
-	$mysql = connect();
-	echo $mysql;
+
+	$mysql = mysqli_connect("localhost","root","C0d1ng4fUn","preguntas_salud");
+	if (mysqli_connect_errno($mysql))
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+
 /*	$results = getPregunta($pregunta);
 	$holi = $results[0].":";
 	foreach ($results[1] as $res) {
