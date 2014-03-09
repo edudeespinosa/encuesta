e5476ac5626e652ca73b5f642dba70ef98a50f1f
edudeespinosa <edude.espinosa@gmail.com>
2014-03-08 20:07:46 -0600

    aaaaaaaaaaaaaaaaagh

--- controller/print_questions.php
+++ controller/print_questions.php
@@ -1,14 +1,14 @@
 <?php
 
 	$pregunta = $_POST["numPregunta"];
-	include_once("../models/modelo.php");
-
+	include_once("/encuesta/models/modelo.php");
+	echo $pregunta;
 	$results = getPregunta($pregunta);
 	$holi = $results[0].":";
 	foreach ($results[1] as $res) {
 		$holi.=$res.";";
 	}
 	$holi = var_dump($holi); 
-	echo ($holi);
+	//echo ($holi);
 
 ?>
