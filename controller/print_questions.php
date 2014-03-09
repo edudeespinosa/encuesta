f337ddc83a749a1fae8eca6a232a8fff4b453950
edudeespinosa <edude.espinosa@gmail.com>
2014-03-09 00:02:24 -0600

    HOLI

--- controller/print_questions.php
+++ controller/print_questions.php
@@ -2,12 +2,13 @@
 
 	$pregunta = $_POST["numPregunta"];
 	include_once("../models/modelo.php");
-
-	$results = getPregunta($pregunta);
+	$mysql = connect();
+	echo $mysql;
+/*	$results = getPregunta($pregunta);
 	$holi = $results[0].":";
 	foreach ($results[1] as $res) {
 		$holi.=$res.";";
 	} 
 	echo ($holi);
-
+*/
 ?>
