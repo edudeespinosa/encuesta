07e64406a68438f875ded128bb384fbcdcfaa5b4
edudeespinosa <edude.espinosa@gmail.com>
2014-03-09 00:13:42 -0600

    asdfasd

--- controller/print_questions.php
+++ controller/print_questions.php
@@ -3,37 +3,11 @@
 	$pregunta = $_POST["numPregunta"];
 	include_once("../models/modelo.php");
 
-	$mysql = mysqli_connect("localhost","root","C0d1ng4fUn","preguntas_salud");
-	if (mysqli_connect_errno($mysql))
-  {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  }
-  		$queryP="SELECT texto FROM preguntas WHERE id=".$pregunta;
-		$queryR="SELECT texto FROM respuesta WHERE id_pregunta=".$pregunta;
-		$resP=$mysql->query($queryP);
-		$resR=$mysql->query($queryR);
-		$resP=mysqli_fetch_array($resP);
-		$respuestas=array();
-
-		while ($row=mysqli_fetch_array($resR,MYSQLI_BOTH)){
-			array_push($respuestas, $row['texto']);
-		}
-		$res=array($resP['texto'],$respuestas);
-		//var_dump($res);
-		disconnect($mysql);
-		echo $res;
-//$results = getPregunta($pregunta);
-	$holi = $res[0].":";
-	foreach ($results[1] as $res2) {
-		$holi.=$res2.";";
-	} 
-	echo ($holi);
-
-/*	$results = getPregunta($pregunta);
+	$results = getPregunta($pregunta);
 	$holi = $results[0].":";
 	foreach ($results[1] as $res) {
 		$holi.=$res.";";
 	} 
 	echo ($holi);
-*/
+
 ?>
