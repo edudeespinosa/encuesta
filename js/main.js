799f53aa9f08c30792b407b2fbcc5237d38874a9
edudeespinosa <edude.espinosa@gmail.com>
2014-03-09 05:03:05 -0600

    holi

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ function cerrar_sesion(event)
 {
 	event.preventDefault();
 	$.ajax({
-		url: './controller/controllerCloseSession.php',
+		url: '../controller/controllerCloseSession.php',
 		type: 'post',
 		success: function (response){
 			window.location.href = "./index.php";
