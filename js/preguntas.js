91462439705b3663dc9b13e432f8bbf6190f7dd3
edudeespinosa <edude.espinosa@gmail.com>
2014-03-08 18:52:39 -0600

    Third line...

--- js/preguntas.js
+++ js/preguntas.js
@@ -21,7 +21,7 @@
 		};
 		$.ajax({
 			data: parametros,
-			url: "<?php echo base_url()?>"+"/controller/print_questions.php",
+			url: "<?php echo site_url('../controller/print_questions.php')?>",
 			type: 'post',
 			beforeSend: function(){
 				$("#pregunta").flippy({
