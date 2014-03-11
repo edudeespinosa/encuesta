825471f1bfd759f5f4c4e9ef87820d59ffb13049
edudeespinosa <edude.espinosa@gmail.com>
2014-03-11 09:47:28 -0600

    holi

--- views/register.php
+++ views/register.php
@@ -14,39 +14,41 @@
 
 </head>
 <body>
-<div id="registro">
-	<!--div id="verificaciones"></div-->
-	<form id="forma_registro" >
-		<h1>¡Registra una nueva cuenta!</h1>
-		<div class="form-group">	
-		<label class="left">Usuario</label>
-		<input class="right" name="user" id="user" type="text" maxlength="15" />
-		</div>
-		<div class="form-group">
-		<label class="left">Password</label>
-		<input class="right" name="password" id="password" type="password" maxlength="15" />
-		</div>
-		<div class="form-group">
-		<label class="left">Reingresa Password</label>
-		<input class="right" name="repassword" id="repassword" type="password" maxlength="15" />
-		</div>
-		<div class="form-group">
-		<label class="left">E-mail</label>
-		<input class="right" name="mail" id="mail" type="text" />
-		</div>
-		<div class="form-group">
-		<label>Sexo</label>
-		<div class="form-group">
-			<li class="radio sexo"><label for="sexo1">Masculino<input type="radio" name="sexo" value="M" id="sexo1"></label></li>
-			<li class="radio sexo"><label for="sexo2">Femenino<input type="radio" name="sexo" value="F" id="sexo2"></label></li>
-		</div>		</div>
-		<div class="form-group">
-		<label class="left">Fecha de Nacimiento</label>
-		<input class="right" name="fecha" id="fecha" type="text" maxlength="15" />
-		</div>
-		<input type="submit" value="Registrarse" name="register" class="btn btn-info" onclick="checkForm($('#forma_registro'))" />
-		<div class="alerts" style="display: none; width:100%;"></div>
-	</form>	
+<div class="wrapper">
+	<div id="registro">
+		<!--div id="verificaciones"></div-->
+		<form id="forma_registro" >
+			<h1>¡Registra una nueva cuenta!</h1>
+			<div class="form-group">	
+			<label class="left">Usuario</label>
+			<input class="right" name="user" id="user" type="text" maxlength="15" />
+			</div>
+			<div class="form-group">
+			<label class="left">Password</label>
+			<input class="right" name="password" id="password" type="password" maxlength="15" />
+			</div>
+			<div class="form-group">
+			<label class="left">Reingresa Password</label>
+			<input class="right" name="repassword" id="repassword" type="password" maxlength="15" />
+			</div>
+			<div class="form-group">
+			<label class="left">E-mail</label>
+			<input class="right" name="mail" id="mail" type="text" />
+			</div>
+			<div class="form-group">
+			<label>Sexo</label>
+			<div class="form-group">
+				<li class="radio sexo"><label for="sexo1">Masculino<input type="radio" name="sexo" value="M" id="sexo1"></label></li>
+				<li class="radio sexo"><label for="sexo2">Femenino<input type="radio" name="sexo" value="F" id="sexo2"></label></li>
+			</div>		</div>
+			<div class="form-group">
+			<label class="left">Fecha de Nacimiento</label>
+			<input class="right" name="fecha" id="fecha" type="text" maxlength="15" />
+			</div>
+			<input type="submit" value="Registrarse" name="register" class="btn btn-info" onclick="checkForm($('#forma_registro'))" />
+			<div class="alerts" style="display: none; width:100%;"></div>
+		</form>	
+	</div>
 </div>
 </body>
 </html>
